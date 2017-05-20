ea01b0d0e0874bf07f1398ca5850f4a12048a85b
haoliangyu <njkon@foxmail.com>
2017-05-20 11:57:51 -0400

    more indexes

--- sql/public/indexes.sql
+++ sql/public/indexes.sql
@@ -2,6 +2,11 @@ CREATE INDEX ON public.dataset (id);
 CREATE INDEX ON public.dataset (portal_dataset_id);
 CREATE INDEX ON public.dataset (version_number);
 CREATE INDEX ON public.portal (id);
+CREATE INDEX ON public.dataset_publisher (name);
+CREATE INDEX ON public.dataset_tag (name);
+CREATE INDEX ON public.dataset_category (name);
+CREATE INDEX ON public.dataset_region USING GIST (geom);
+
 CREATE UNIQUE INDEX ON public.dataset (portal_id, portal_dataset_id, updated_time);
 CREATE UNIQUE INDEX ON public.dataset_tag (name);
 CREATE UNIQUE INDEX ON public.dataset_category (name);
