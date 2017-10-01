d1dad21ac13f640f5382eaa23b1027032b89e6a2
haoliangyu <njkon@foxmail.com>
2017-10-01 00:32:00 -0400

    add indexes and simplify materialized views

--- sql/public/indexes.sql
+++ sql/public/indexes.sql
@@ -2,16 +2,14 @@
  * Table indexes
  */
 CREATE INDEX ON public.dataset (id);
+CREATE INDEX ON public.dataset (portal_dataset_id);
 CREATE INDEX ON public.dataset (identifier);
 CREATE INDEX ON public.dataset (version);
-CREATE INDEX ON public.portal (id);
-CREATE INDEX ON public.dataset_publisher (name);
+CREATE INDEX ON public.dataset_portal_xref (dataset_id);
+CREATE INDEX ON public.dataset_publisher_xref (dataset_id);
 CREATE INDEX ON public.dataset_keyword_xref(dataset_id);
-CREATE INDEX ON public.dataset_keyword (name);
 CREATE INDEX ON public.dataset_theme_xref(dataset_id);
-CREATE INDEX ON public.dataset_theme (name);
 CREATE INDEX ON public.dataset_distribution (dataset_id);
-CREATE INDEX ON public.dataset_coverage USING GIST (geom);
 
 /**
  * Table unique indexes
@@ -19,14 +17,3 @@ CREATE INDEX ON public.dataset_coverage USING GIST (geom);
 CREATE UNIQUE INDEX ON public.dataset_keyword (name);
 CREATE UNIQUE INDEX ON public.dataset_theme (name);
 CREATE UNIQUE INDEX ON public.dataset_publisher (name);
-
-/**
- * Materialized view indexes
- */
-CREATE INDEX ON public.mview_latest_dataset (identifier);
-
-/**
- * Materialized view unique indexes
- */
-CREATE UNIQUE INDEX ON public.mview_latest_dataset (id);
-CREATE UNIQUE INDEX ON public.mview_portal (id);
--- sql/public/material_views.sql
+++ sql/public/material_views.sql
@@ -1,43 +1,20 @@
 CREATE MATERIALIZED VIEW public.mview_latest_dataset AS
-  SELECT
-    view_latest_dataset.id,
-    view_latest_dataset.identifier,
-    view_latest_dataset.title,
-    view_latest_dataset.description,
-    view_latest_dataset.publisher,
-    view_latest_dataset.portal_id,
-    view_latest_dataset.portal,
-    view_latest_dataset.portal_url,
-    view_latest_dataset.platform,
-    view_latest_dataset.landing_page,
-    view_latest_dataset.issued,
-    view_latest_dataset.modified,
-    view_latest_dataset.license,
-    view_latest_dataset.spatial,
-    view_latest_dataset.version,
-    view_latest_dataset.version_period,
-    view_latest_dataset.version_history,
-    view_latest_dataset.distribution,
-    view_latest_dataset.keyword,
-    view_latest_dataset.theme
-  FROM view_latest_dataset
-  WITH DATA;
+  SELECT DISTINCT ON (d.portal_dataset_id, dpx.portal_id)
+    d.id,
+    d.identifier,
+    dpx.portal_id,
+    d.modified
+  FROM dataset AS d
+  LEFT JOIN dataset_portal_xref AS dpx ON dpx.dataset_id = d.id
+  ORDER BY d.portal_dataset_id, dpx.portal_id, d.modified DESC;
 
 CREATE MATERIALIZED VIEW public.mview_portal AS
   WITH data_summary AS (
     SELECT
       sd.portal_id,
-      COUNT(sd.portal_id),
+      COUNT(sd.id),
       MAX(sd.modified) AS modified
-    FROM (
-      SELECT DISTINCT ON (dpx.portal_id, d.identifier)
-        d.identifier,
-        dpx.portal_id,
-        d.modified
-      FROM dataset_portal_xref AS dpx
-      LEFT JOIN dataset AS d ON dpx.dataset_id = d.id
-      ORDER BY d.portal_id, d.identifier, d.version DESC
-    ) sd
+    FROM public.mview_latest_dataset AS sd
     GROUP BY sd.portal_id
   )
   SELECT
